6990d82e32cae38f307d509d708e15dbe8f02e6d
summorey22 <sumitmorey247@gmail.com>
2023-04-24 10:43:12 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/books')
+def books():
+    return render_template('books.html')
---
+++ static/js/script.js
@@ -0,0 +1,25 @@
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+let plant = document.getElementById('plant');
+
+let height = document.body.scrollHeight;
+
+document.addEventListener('scroll', () => {
+    let value =  window.scrollY;
+    console.log(value + ' : ' + height);
+
+    if(value <= height - 2*value) {
+        text.style.marginTop = value * 2.5 + 'px';
+        leaf.style.top = value *-1.5 + 'px';
+        leaf.style.left = value *1.5 + 'px';
+        hill5.style.left = value *1.5 + 'px';
+        hill4.style.left = value *-1.5 + 'px';
+        hill1.style.top = value * 1 + 'px';
+    }
+})
+
+document.addEventListener('scrollend', () => { })
+// window.scrollByPages(2)
